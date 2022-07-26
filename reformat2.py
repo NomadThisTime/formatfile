b03de1f832d6046d3104050d50ebe7e41e6b7c9b
Nomad <jakubkostrzebski@gmail.com>
2022-07-26 11:12:07 +0200

    Fixed formatting of pfam_all.json

--- reformat2.py
+++ reformat2.py
@@ -3,6 +3,6 @@ with open("newfile.txt", "r") as f:
 
 with open("pfam_all.json", "w") as f:
     for line in lines:
-        new_string = ('{"text: "' + line[1:-1] + '"}\n')
+        new_string = ('{"text": "' + line[1:-1] + '"}\n')
         f.write(new_string)
 # remove single line at the end

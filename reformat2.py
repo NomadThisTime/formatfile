b2b7dd4f1cdaf496d86ed52e41249e175af4df21
Nomad <jakubkostrzebski@gmail.com>
2022-07-26 10:43:17 +0200

    Finalized making the .json file for ProteinLM

---
+++ reformat2.py
@@ -0,0 +1,8 @@
+with open("newfile.txt", "r") as f:
+    lines = f.readlines()
+
+with open("pfam_all.json", "w") as f:
+    for line in lines:
+        new_string = ('{"text: "' + line[1:-1] + '"}\n')
+        f.write(new_string)
+# remove single line at the end
--- reformat5k.py
+++ reformat5k.py
@@ -1,8 +1,13 @@
+# reformat data_file.fasta to have 5000 amino acids with spaces between each letter
+
 with open("data_file.fasta", "r") as f:
-    #lines = f.readlines()
     lines = [next(f) for x in range(10000)]
 
 with open("newfile.txt", "w") as f:
     for line in lines:
         if not line.strip("\n").startswith(">"):
-            f.write(line)
+            for character in line[1:]:
+                f.write(character + " ")
+
+# add a single space in line 1 after
+# remove single line at the end
